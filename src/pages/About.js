c58ce8cce9b1624b86743d185d1cf8312ec50fa4
Ajay Bhatia <Ajaybhatiawork2020@gmail.com>
2022-06-09 17:50:46 -0700

    starting!

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import ProfilePic from '../assets/profilePic.jpg';
   
 export default class About extends Component {
   render() {
     return (
-      <div>
-        <p>Design your About me page </p> 
+      <div class="about">
+        <div class="profilePic">
+          <img class="pic" src={ProfilePic} alt="Profile Pic"/>
+        </div>
+        <div class="name">
+          <h4>Ajay Bhatia</h4>
+        </div>
+        
       </div>
     )
   }
